2a322490e04f35afdcc99a87a7d28fbbc2113c8f
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-23 17:04:53 +0300

    programmatic navigation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-function Login({ setIsLoggedIn }) {
-  const history = useHistory();
+function Login({ onLogin }) {
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
+  const history = useHistory();
 
   function handleChange(e) {
     setFormData({
@@ -17,16 +17,23 @@ function Login({ setIsLoggedIn }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-
-    setIsLoggedIn(true);
-
-    // after logging the user in, redirect to the home page!
-    history.push("/");
+    fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then((user) => {
+        onLogin(user);
+        // after logging the user in, redirect to the home page!
+        history.push("/home");
+      });
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
       <input
         type="text"
         name="username"
@@ -43,5 +50,48 @@ function Login({ setIsLoggedIn }) {
     </form>
   );
 }
+
+// function Login({ setIsLoggedIn }) {
+//   const history = useHistory();
+//   const [formData, setFormData] = useState({
+//     username: "",
+//     password: "",
+//   });
+
+//   function handleChange(e) {
+//     setFormData({
+//       ...formData,
+//       [e.target.name]: e.target.value,
+//     });
+//   }
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+
+//     setIsLoggedIn(true);
+
+//     // after logging the user in, redirect to the home page!
+//     history.push("/");
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <h1>Login</h1>
+//       <input
+//         type="text"
+//         name="username"
+//         value={formData.username}
+//         onChange={handleChange}
+//       />
+//       <input
+//         type="password"
+//         name="password"
+//         value={formData.password}
+//         onChange={handleChange}
+//       />
+//       <button type="submit">Login</button>
+//     </form>
+//   );
+// }
 
 export default Login;
